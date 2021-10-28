a51239eef6cbae8e5cf2d5036536fb9181bcf7c3
Jeferson Vivas <57049083+jefvivas@users.noreply.github.com>
2021-10-28 12:17:41 -0300

    Começo do estudo

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Equipe = (props)=>{
+  return(
+    <div>
+      <Sobre nome={props.nome} cargo={props.cargo}
+        idade={props.idade}/>
+      <Social fb={props.facebook}/>
+      <hr/>
     </div>
-  );
+  )
+  }
+const Sobre = (props)=>{
+  return(
+    <div>
+      <h2>Olá sou o {props.nome}</h2>
+      <h3>Cargo: {props.cargo}</h3>
+      <h3>Idade: {props.idade} anos</h3>
+    </div>
+  )
 }
 
-export default App;
+const Social = (props)=>{
+  return(
+    <div>
+      <a href={props.fb}>Facebook </a>
+      <a>LinkedIn </a>
+      <a>Youtube </a>
+    </div>
+  )
+}
+
+
+function App(){
+  return(
+    <div>
+      <h1>Conheça nossa equipe</h1>
+      <Equipe nome='Lucas' cargo='Programador' idade='29' facebook='www.facebook.com/oi'/>
+      <Equipe nome='José' cargo='Designer' idade='33'/>
+    </div>
+  )
+}
+
+export default App
