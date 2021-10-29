d43042ce601d7ad0576a30b505e54df88ffcbbeb
Jeferson Vivas <57049083+jefvivas@users.noreply.github.com>
2021-10-28 22:57:44 -0300

    update react

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,44 +1,34 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-const Equipe = (props)=>{
-  return(
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo}
-        idade={props.idade}/>
-      <Social fb={props.facebook}/>
-      <hr/>
-    </div>
-  )
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      hora:'00:00:00'
+    }
   }
-const Sobre = (props)=>{
-  return(
-    <div>
-      <h2>Olá sou o {props.nome}</h2>
-      <h3>Cargo: {props.cargo}</h3>
-      <h3>Idade: {props.idade} anos</h3>
-    </div>
-  )
-}
+  
+  componentDidMount(){
 
-const Social = (props)=>{
-  return(
-    <div>
-      <a href={props.fb}>Facebook </a>
-      <a>LinkedIn </a>
-      <a>Youtube </a>
-    </div>
-  )
-}
+    setInterval(()=>{
+      this.setState({hora: new Date().toLocaleTimeString()})
+    },1000)
 
+  }
 
-function App(){
-  return(
-    <div>
-      <h1>Conheça nossa equipe</h1>
-      <Equipe nome='Lucas' cargo='Programador' idade='29' facebook='www.facebook.com/oi'/>
-      <Equipe nome='José' cargo='Designer' idade='33'/>
-    </div>
-  )
+  componentDidUpdate(){
+    console.log('Atualizou')
+  }
+  
+
+  render(){
+    return(
+      <div>
+        <h1>Projetinho {this.state.hora}</h1>
+      </div>
+    )
+  }
 }
 
 export default App
